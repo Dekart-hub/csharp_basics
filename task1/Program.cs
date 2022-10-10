40fdac174abdc6204ad1a72f9cf097a36332f7fd
Dekart <dekarter@protonmail.com>
2022-10-10 16:54:29 +0300

    init commit

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileCont = File.ReadAllText(@"../1.dat");
+            string[] numSplit = fileCont.Split(new char[]('', '\t', '\r', '\n'), StringSplitOptions.RemoveEmptyEntries);
+            int[] integers = new int[numSplit.Length];
+            for(int i = 0; i < numSplit.Length; i++)
+            {
+                integers[i] = int.Parse(numSplit[i]);
+            }
+            
+        }
+    }
+}
