a79357b02a0c456564007ddf311acbbfc11ca5ab
Dekart <dekarter@protonmail.com>
2022-10-13 20:19:23 +0300

    Добавлены решения task1 и task2 для бинарных файлов

--- task1/Program.cs
+++ task1/Program.cs
@@ -9,11 +9,11 @@ namespace FileTask1
 class Prog
 {
     const int POINTS_TO_PASS = 56;
-    static void Main()
+    const string  INPUT_FILE_PATH = @"../input.txt";
+    
+    static void Task1()
     {
-        string inputFilePath = @"../input.txt";
-        string[] students = File.ReadAllLines(inputFilePath);
-
+        string[] students = File.ReadAllLines(INPUT_FILE_PATH);
         string[] studentInf;
         for(int i = 0; i < students.Length; i++)
         {
@@ -22,11 +22,46 @@ class Prog
             if(int.Parse(studentInf[3]) < POINTS_TO_PASS || int.Parse(studentInf[4]) < POINTS_TO_PASS
                          || int.Parse(studentInf[5]) < POINTS_TO_PASS)
             {
-                Console.WriteLine("не закрыл сессию!");
+                Console.WriteLine("не сдал сессию!");
             } else {
-                Console.WriteLine("закрыл сессию.");
+                Console.WriteLine("сдал сессию.");
             }
         } 
     }
+
+    static void task2()
+    {
+        string[] content = File.ReadAllLines(INPUT_FILE_PATH);
+        int n = 0;
+        int count = 0;
+
+        while(n != content.Length)
+        {
+            count = int.Parse(content[n]);
+            for(int i = 0; i < count; i++)
+            {
+                for(int j = 0; j < count; j++)
+                {
+
+                }
+            }
+
+        }
+        // using(FileStream fr = new FileStream(@"../input.txt", FileMode.Open))
+        // {
+        //   for(int offset=1; offset <= fr.Length; offset++)
+        //         {
+        //             fr.Seek(-offset, SeekOrigin.End);
+                   
+        //             fw.WriteByte((byte)fr.ReadByte());
+        //         }
+        // }
+    }
+
+    static void Main()
+    {
+        Task1();
+    }
+    
 }
 }
---
+++ task2/Program.cs
@@ -0,0 +1,105 @@
+﻿
+
+namespace task2
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        BinaryWriter bw = new BinaryWriter(File.Open("1.bin", FileMode.Create));
+        Random r = new Random();
+        for(int i=0; i<20; i++)
+        {
+            bw.Write(r.Next(20));
+            Console.Write($"{r} ");
+        }
+        bw.Close();
+
+        task2();
+    }
+
+    static void task1()
+    {
+        using (BinaryReader br = new BinaryReader(new FileStream("1.bin", FileMode.Open)))
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            long maxPos = br.BaseStream.Position;
+            long minPos = br.BaseStream.Position; 
+            try
+            {
+                    
+                while (true)
+                {
+                    int num = br.ReadInt32();
+                    if(num > max)
+                    {
+                        maxPos = br.BaseStream.Position;
+                        max = num;
+                    } else if (num < min)
+                    {
+                        minPos = br.BaseStream.Position;
+                        min = num;
+                    }
+                }
+            }
+            catch(EndOfStreamException e) {
+                long left = minPos;
+                long right = maxPos;
+                if(minPos > maxPos)
+                {
+                    left = maxPos;
+                    right = minPos;
+                }
+                br.BaseStream.Position = left;
+                int sum = 0;
+                while(br.BaseStream.Position != right)
+                {
+                    int num = br.ReadInt32();
+                    sum += num;
+                } 
+                Console.WriteLine($"\n sum={sum}");
+            }
+        }
+    }
+
+    static void task2()
+    {
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        long maxPos = 0;
+        long minPos = 0;  
+        using (BinaryReader br = new BinaryReader(new FileStream("1.bin", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite)))
+        {
+            maxPos = br.BaseStream.Position;
+            minPos = br.BaseStream.Position;
+            try
+            {   
+                while (true)
+                {
+                    int num = br.ReadInt32();
+                    if(num > max)
+                    {
+                        maxPos = br.BaseStream.Position;
+                        max = num;
+                    } else if (num < min)
+                    {
+                        minPos = br.BaseStream.Position;
+                        min = num;
+                    }
+                }
+            } catch(EndOfStreamException e) {
+            }
+
+            try{
+                int num = br.ReadInt32();
+                Console.Write($"{num} ");                
+            } catch(EndOfStreamException e) {
+                Console.Write($"\n");
+            }
+        }
+        
+    }
+
+}
+}
